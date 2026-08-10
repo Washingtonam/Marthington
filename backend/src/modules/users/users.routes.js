9bf0071db0cadf9e100da8f73275689828ffc9ef
Washington Amedu <washingtonamedu@gmail.com>
2026-08-10 23:51:01 +0100

    feat: Add full landing page code

--- backend/src/modules/users/users.routes.js
+++ backend/src/modules/users/users.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import protect from "../../middlewares/auth.middleware.js";
+import checkPermission from "../../middlewares/permission.middleware.js";
 import {
   createStaff,
   getStaff,
